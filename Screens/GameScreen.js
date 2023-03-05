2417118fe7874d5bf0c62f83811b53f0e822c713
KhanhDuyDang91 <khanhduydang910@gmail.com>
2023-03-05 21:12:33 +0700

    Styling ListItem

--- Screens/GameScreen.js
+++ Screens/GameScreen.js
@@ -27,6 +27,13 @@ const generateRandomBetween = (min, max, exclude) => {
   }
 };
 
+const renderListItem = (value, numOfRound) => (
+  <View key={value} style={styles.listItem}>
+    <BodyText>#Round {numOfRound}</BodyText>
+    <BodyText>{value}</BodyText>
+  </View>
+);
+
 const GameScreen = (props) => {
   const initialGuess = generateRandomBetween(1, 100, props.userChoice);
   const [currentGuess, setCurrentGuess] = useState(initialGuess);
@@ -82,13 +89,13 @@ const GameScreen = (props) => {
           <Ionicons name="caret-up-circle-outline" size={24} color="white" />
         </MainButton>
       </Card>
-      <ScrollView>
-        {pastGuesses.map((guess) => (
-          <View key={guess}>
-            <Text>{guess}</Text>
-          </View>
-        ))}
-      </ScrollView>
+      <View style={styles.list}>
+        <ScrollView>
+          {pastGuesses.map((guess, index) =>
+            renderListItem(guess, pastGuesses.length - index)
+          )}
+        </ScrollView>
+      </View>
     </View>
   );
 };
@@ -111,6 +118,19 @@ const styles = StyleSheet.create({
   },
   Secondary: {
     color: Colors.accent,
+  },
+  list: {
+    flex: 1,
+    width: "80%",
+  },
+  listItem: {
+    borderColor: "#ccc",
+    borderWidth: 1,
+    padding: 15,
+    marginVertical: 10,
+    backgroundColor: "white",
+    flexDirection: "row",
+    justifyContent: "space-between",
   },
 });
 
