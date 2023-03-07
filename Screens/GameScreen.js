cb7fdd5a1317608c5c122ce595a5886febfa3080
KhanhDuyDang91 <khanhduydang910@gmail.com>
2023-03-07 16:44:02 +0700

    Using Dimension API

--- Screens/GameScreen.js
+++ Screens/GameScreen.js
@@ -7,6 +7,7 @@ import {
   Alert,
   ScrollView,
   FlatList,
+  Dimensions,
 } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 
@@ -120,7 +121,7 @@ const styles = StyleSheet.create({
   buttonContainer: {
     flexDirection: "row",
     justifyContent: "space-around",
-    marginTop: 20,
+    marginTop: Dimensions.get("window").height > 600 ? 20 : 5,
     width: 400,
     maxWidth: "90%",
   },
@@ -132,7 +133,7 @@ const styles = StyleSheet.create({
   },
   listContainer: {
     flex: 1,
-    width: "85%",
+    width: Dimensions.get("window").width > 350 ? "70%" : "90%",
     backgroundColor: "white",
     elevation: 5,
     paddingHorizontal: 10,
--- Screens/StartGameScreen.js
+++ Screens/StartGameScreen.js
@@ -7,6 +7,7 @@ import {
   TouchableWithoutFeedback,
   Keyboard,
   Alert,
+  Dimensions,
 } from "react-native";
 import { useState } from "react";
 
@@ -113,8 +114,9 @@ const styles = StyleSheet.create({
     marginVertical: 10,
   },
   inputContainer: {
-    width: 300,
-    maxWidth: "80%",
+    width: "80%",
+    maxWidth: "95%",
+    minWidth: 300,
     alignItems: "center",
   },
   buttonContainer: {
@@ -124,7 +126,7 @@ const styles = StyleSheet.create({
     paddingHorizontal: 15,
   },
   button: {
-    width: 100,
+    width: Dimensions.get("window").width / 4,
   },
   textInput: {
     textAlign: "center",
